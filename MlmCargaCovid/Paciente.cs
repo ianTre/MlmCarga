af7ed20a58b9c43bd816ef10be8812e21286815b
ian <ian>
2020-07-28 13:29:32 -0300

    First Commit

---
+++ MlmCargaCovid/Paciente.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace MlmCargaCovid
+{
+    class Paciente
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int NumeroDoc { get; set; }
+        public int Edad { get; set; }
+        public DateTime FechaCarga { get; set; }
+        public string Telefono { get; set; }
+
+
+        public Paciente(DataRow row)
+        {
+            var columns = row.ItemArray;
+
+            Nombre = columns[0].ToString();
+            if (String.IsNullOrEmpty(Nombre))
+                throw new Exception("El registro no contiene nombre");
+
+            Apellido = columns[1].ToString();
+            if (String.IsNullOrEmpty(Apellido))
+                throw new Exception("El registro no contiene Apellido");
+
+            int aux;
+            string doc =columns[2].ToString();
+            if(String.IsNullOrEmpty(doc))
+                throw new Exception("El registro no contiene DNI");
+
+            if(!int.TryParse(doc,out aux))
+                throw new Exception("El registro no contiene DNI");
+            NumeroDoc = aux;
+
+            Edad = 0;
+            int.TryParse(columns[3].ToString(), out aux);
+            Edad = aux;
+
+
+            string fecha = columns[9].ToString();
+            DateTime auxFecha;
+            DateTime.TryParse(fecha, out auxFecha);
+            FechaCarga = auxFecha;
+
+
+            Telefono = columns[22].ToString();
+
+
+
+        }   
+    }
+
+}
---
+++ MlmCargaCovid/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace MlmCargaCovid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hola mundo");
+            var data = ReadExcelFile("BASE", @"D:\Codigo\MlmCargaCovid\MlmCargaCovid\ExcelSISA.xlsx");
+            List<Paciente> pacientes = new List<Paciente>();
+            foreach (DataRow row in data.Rows)
+            {
+                Paciente persona = new Paciente(row);
+                pacientes.Add(persona);
+            }
+
+
+        }
+
+
+        static private DataTable ReadExcelFile(string sheetName, string path)
+        {
+
+            using (OleDbConnection conn = new OleDbConnection())
+            {
+                DataTable dt = new DataTable();
+                string Import_FileName = path;
+                string fileExtension = Path.GetExtension(Import_FileName);
+                if (fileExtension == ".xls")
+                    conn.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Import_FileName + ";" + "Extended Properties='Excel 8.0;HDR=YES;'";
+                if (fileExtension == ".xlsx")
+                    conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Import_FileName + ";" + "Extended Properties='Excel 12.0 Xml;HDR=YES;'";
+                using (OleDbCommand comm = new OleDbCommand())
+                {
+                    comm.CommandText = "Select * from [" + sheetName + "$]";
+
+                    comm.Connection = conn;
+
+                    using (OleDbDataAdapter da = new OleDbDataAdapter())
+                    {
+                        da.SelectCommand = comm;
+                        da.Fill(dt);
+                        return dt;
+                    }
+
+                }
+            }
+        }
+    }
+}
